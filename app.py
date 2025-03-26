352c273796949fa9e8c3b12e7eb2641be0b7589c
Stef Meden <58486072+smeden-lod@users.noreply.github.com>
2025-03-26 22:13:59 +0100

    Update app.py
    new version of app.py

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 app = Flask(__name__, static_url_path='', static_folder="./static")
 
 
+
 with open(os.path.join(SITE_ROOT,"videos.json"), "r") as videos_database_file:
     videos_database = json.load(videos_database_file)
 
+def videos_database_write() -> bool:
+    with open(os.path.join(SITE_ROOT,"videos.json"), "w") as videos_database_file:
+        json.dump(user_database, videos_database, videos_database_file)
 
 with open(os.path.join(SITE_ROOT,"clips.json"), "r") as clips_database_file:
     clips_database = json.load(clips_database_file)
@@ -24,11 +28,8 @@ def composer():
 
 @app.route("/consulter")
 def consulter(): 
-    return render_template("consulter.html", chors = videos_database)
+    return render_template("consulter.html", chors = videos_database[0]["bibli"])
 
-@app.route("/accueil")
-def accueil():
-    return render_template("accueil.html")
       
 @app.route("/play/<int:code>")
 def play(code):
