7c1ba980907da982f40afacc2d0f7e6862d916cf
Stef Meden <58486072+smeden-lod@users.noreply.github.com>
2025-03-27 20:13:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 app = Flask(__name__, static_url_path='', static_folder="./static")
 
 
-with open(os.path.join(SITE_ROOT,"videos.json"), "r") as videos_database_file:
-    videos_database = json.load(videos_database_file)
 
 
 @app.route("/")
@@ -17,6 +15,15 @@ def index():
 @app.route("/composer")
 def composer(): 
     return render_template("composer.html")
+
+@app.route("/save", methods=['POST'])
+def save():
+    d = request.form['code_chor']
+    with open(os.path.join(SITE_ROOT,"videos.json"), "r") as f:
+        b = json.load(f)
+    b.append({"id_chor": len(b)+1, "code_chor": d})
+     with open(os.path.join(SITE_ROOT,"videos.json"), "w") as f:
+        json.dump(b, f)   
 
 @app.route("/consulter")
 def consulter():
