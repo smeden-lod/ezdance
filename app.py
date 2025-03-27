729568bda2c1da5d45035fd7a587933370b34852
Nekosama20 <dangerouspig3@gmail.com>
2025-03-27 09:32:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,14 +24,14 @@ def index():
 
 @app.route("/composer")
 def composer(): 
-    return render_template("composer.html")
-
-@app.route("/consulter")
-def consulter(): 
     user_name = request.form['username']
     for x in videos_database:
         if x["id_user"] == user_name:
             x['bibli'].append({"id_chor" : len(x['bibli']) + 1, "code_chor" : "007"})
+    return render_template("composer.html")
+
+@app.route("/consulter")
+def consulter():
     return render_template("consulter.html", chors = videos_database[0]["bibli"])
 
       
